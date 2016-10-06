2f78dd9aa9f2bb6ab574845b6ffe067de7c7fdeb
Amir Goldstein <amir73il@gmail.com>
2016-10-06 18:36:37 +0300

    add rename to test script

--- test_demo.sh
+++ test_demo.sh
@@ -1,3 +1,4 @@
+#!/bin/sh -x
 echo "file fs/notify/fanotify/*  +p" > /sys/kernel/debug/dynamic_debug/control
 mkdir -p a/b/c/d/e/f/g/
 mkdir -p /tmp/b
@@ -7,14 +8,20 @@ echo 3 > /proc/sys/vm/drop_caches
 sleep 1
 touch a/0
 chmod +x a/0
-mkdir a/dir
+mkdir a/dir0
 sleep 1
-rm a/0
-rmdir a/dir
+mv a/0 a/1
+mv a/dir0 a/dir1
+sleep 1
+rm a/1
+rmdir a/dir1
 sleep 1
 touch /tmp/b/c/d/e/f/0
 chmod +x /tmp/b/c/d/e/f/0
 sleep 1
-rm /tmp/b/c/d/e/f/0
-rmdir /tmp/b/c/d/e/f/g/
+mv /tmp/b/c/d/e/f/0 /tmp/b/c/d/e/f/1
+mv /tmp/b/c/d/e/f/g /tmp/b/c/d/e/f/G
+sleep 1
+rm /tmp/b/c/d/e/f/1
+rmdir /tmp/b/c/d/e/f/G
 
