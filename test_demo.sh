1314a0cbb1dd1e3b94790ccd2dead5ecba4973c2
Amir Goldstein <amir73il@gmail.com>
2016-10-06 16:53:31 +0300

    update test script to run fanotify_demo

--- test_demo.sh
+++ test_demo.sh
@@ -1,2 +1,20 @@
+echo "file fs/notify/fanotify/*  +p" > /sys/kernel/debug/dynamic_debug/control
 mkdir -p a/b/c/d/e/f/g/
-./demo_inotify a a/b a/b/c a/b/c/d a/b/c/d/e/ a/b/c/d/e/f a/b/c/d/e/f/g
+mkdir -p /tmp/b
+mount -o bind a/b /tmp/b
+echo 3 > /proc/sys/vm/drop_caches
+./fanotify_demo a/ &
+sleep 1
+touch a/0
+chmod +x a/0
+mkdir a/dir
+sleep 1
+rm a/0
+rmdir a/dir
+sleep 1
+touch /tmp/b/c/d/e/f/0
+chmod +x /tmp/b/c/d/e/f/0
+sleep 1
+rm /tmp/b/c/d/e/f/0
+rmdir /tmp/b/c/d/e/f/g/
+
