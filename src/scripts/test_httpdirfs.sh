61bedf155dfe0d216aadac499a2ada0feaad3c31
Amir Goldstein <amir73il@gmail.com>
2022-10-20 15:36:06 +0300

    test_httpdirfs.sh: add evict file example
    This command will fail if the file is open by another process.

--- src/scripts/test_httpdirfs.sh
+++ src/scripts/test_httpdirfs.sh
@@ -5,6 +5,7 @@ cp -a /vtmp/libcurl* /lib/x86_64-linux-gnu/
 cp -a /vtmp/libgumbo* /lib/x86_64-linux-gnu/
 cp -a /vtmp/libubsan* /lib/x86_64-linux-gnu/
 cp -a /vtmp/ca-certificates.crt /etc/ssl/certs/
+cp -a /vtmp/httpdirfs /sbin/
 
 MNT=/mnt/www
 umount /vdf 2>/dev/null
@@ -20,9 +21,13 @@ echo "file fs/namei.c -p" > /sys/kernel/debug/dynamic_debug/control
 echo "file fs/notify/fsnotify.c -p" > /sys/kernel/debug/dynamic_debug/control
 echo "file fs/notify/fanotify/* -p" > /sys/kernel/debug/dynamic_debug/control
 
+echo "Examples download on access:"
+echo "md5sum /mnt/www/linux/kernel/firmware/sha256sums.asc"
 echo "time tar tvfz $MNT/linux/kernel/firmware/linux-firmware-20220815.tar.gz |head"
+echo
+echo "Examples httpdirfs commands:"
+echo "evict /mnt/www/linux/kernel/firmware/sha256sums.asc"
+echo
 
 cd /vtmp
-# Uncomment to use filesystem mark
-#MNT=-
-./httpdirfs -d --fanotify --dl-seg-size 1 --cache-location /vdf/cache https://cdn.kernel.org/pub/ $MNT &
+/sbin/httpdirfs -d --fanotify --dl-seg-size 1 --cache-location /vdf/cache https://cdn.kernel.org/pub/ $MNT
